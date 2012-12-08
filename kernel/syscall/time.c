2b5acfb63e1feebb639e5f14b7d466bac8e22469
wwestlin <wwestlin@wwestlin-PC.(none)>
2012-12-08 13:21:33 -0500

    Switching to working11 version

--- kernel/syscall/time.c
+++ kernel/syscall/time.c
@@ -61,6 +61,7 @@ void irq_handler(void) {
 	unsigned int actual_time = current_time/OSTMR_FREQ;
 	dev_update(actual_time);
 	reg_set(OSTMR_OSSR_ADDR, OSTMR_OSSR_M0);
+	current_time = reg_read(OSTMR_OSCR_ADDR);
 	reg_write(OSTMR_OSMR_ADDR(0), current_time + (OSTMR_FREQ/100));
 
 	dispatch_save();
